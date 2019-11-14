070e111e1da8e720a20a8e68d107265444566a69
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-11-14 15:30:19 +0900

    실습4

--- main.c
+++ main.c
@@ -5,18 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	char buffer[8];
-	double *pd;
-	int *pi;
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
 	
-	pd = (double*)buffer;
-	*pd = 3.14;
-	printf("%f\n", *(double*)buffer);
+	*p = 200;
 	
-	pi = (int*)buffer;
-	*pi = 123;
-	*(pi+1) = 456;
-	printf("%d %d\n", *(int*)buffer, *((int*)buffer+1));
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+	
+	**q = 300;
+	
+	printf("i = %d, *p = %d, **q = %d", i, *p, **q);
 	
 	return 0;
 }
